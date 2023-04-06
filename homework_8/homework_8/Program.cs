c08ce9e5ad33d55d00f36bba7add212a46321044
axrorbek2006 <122671790+axrorbek2006@users.noreply.github.com>
2023-04-06 22:03:07 +0500

    task_2
    none

--- homework_8/homework_8/Program.cs
+++ homework_8/homework_8/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-
+            Console.Write(" enter a number :");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($" {num} soni { findSizeNum(num)} xonali son ");
+        }
+        static int findSizeNum(int num)
+        {
+            return num.ToString().Length;          
         }
     }
 }
