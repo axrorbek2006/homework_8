be210c4ecd0a4eb85e3f022bd2d15567b613f756
axrorbek2006 <122671790+axrorbek2006@users.noreply.github.com>
2023-04-06 21:17:06 +0500

    funksiya_2
    metodlar bilan ishlash

--- homework_8/homework_8/Program.cs
+++ homework_8/homework_8/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
+            int num1 = 0;
+            int num2;
+            returnNum(ref num1, out num2);
+            Console.WriteLine("----");
+            Console.WriteLine(num1 + 1);
+            Console.WriteLine("----");
+            Console.WriteLine(num2 + 1);
 
+        }
+        static int returnNum(ref int num1, out int num2)
+        {
+            Console.WriteLine("enter first number : ");
+            bool isnumber = int.TryParse(Console.ReadLine(), out num1);
+            while (!isnumber)
+            {
+                Console.WriteLine("error : ");
+                Console.WriteLine("enter first number : ");
+                isnumber = int.TryParse(Console.ReadLine(), out num1);
+            }
+            Console.WriteLine("enter second num : ");
+            bool isnumber2 = int.TryParse(Console.ReadLine(), out num2);
+            while (!isnumber2)
+            {
+                Console.WriteLine("error : ");
+                Console.WriteLine("enter second number : ");
+                isnumber2 = int.TryParse(Console.ReadLine(), out num2);
+            }
+            return num1;
         }
     }
 }
