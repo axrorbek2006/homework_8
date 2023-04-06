0211f2d6939da42bb6b7b7ed9cd9682430f5c908
axrorbek2006 <122671790+axrorbek2006@users.noreply.github.com>
2023-04-06 22:30:21 +0500

    task_3
    none

--- homework_8/homework_8/Program.cs
+++ homework_8/homework_8/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-
+            Console.Write(" enter x : ");
+            int numx = Convert.ToInt32(Console.ReadLine());
+            Console.Write(" enter y : ");
+            int numy = Convert.ToInt32(Console.ReadLine());
+            minAndMax(ref numx, ref numy);  
         }
+        static int minAndMax(ref int numx , ref int numy)
+        {
+            int x = numx, y = numy;
+            numx = Math.Max(x, y);
+            numy = Math.Min(x, y);
+            Console.WriteLine($" Max : {numx} ");
+            Console.WriteLine($" Min : {numy} ");
+
+            return numx;
+        }
+        
     }
 }
