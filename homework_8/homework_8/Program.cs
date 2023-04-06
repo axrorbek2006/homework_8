4f06228043688e855c336a3f5888e880569436dc
axrorbek2006 <122671790+axrorbek2006@users.noreply.github.com>
2023-04-06 21:38:31 +0500

    task_1
    none

--- homework_8/homework_8/Program.cs
+++ homework_8/homework_8/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-
+            
+            int num = inputNum();
+            findOutNum(num);
+  
+        }
+        static int inputNum()
+        {           
+            Console.Write( " enter a number :" );
+            bool isNumber = int.TryParse( Console.ReadLine(), out int num );
+            while(!isNumber)
+            {
+                Console.WriteLine(" error !");
+                Console.Write(" enter a number :");
+                isNumber = int.TryParse(Console.ReadLine(), out num);              
+            }
+            return num;
+        } 
+        static void findOutNum(int num)
+        {
+            if(num > 0)
+            {
+                Console.WriteLine($"{num } musbat son ");
+            }else if(num < 0 )
+            {
+                Console.WriteLine($"{num} manfiy son ");
+            }else
+            {
+                Console.WriteLine($"{num} manfiy ham musbat ham emas ");
+            }
         }
     }
 }
